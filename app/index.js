d3ede41c7d1ae2403972baeebc9c05e8ec991470
Hackprakas <vijayatrprakash@gmail.com>
2024-03-28 20:34:07 +0530

    animation added

--- app/index.js
+++ app/index.js
@@ -1,89 +1,120 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Image } from "react-native";
 import sunicon from "../assets/sun.gif";
+import LottieView from "lottie-react-native";
+import json from "../assets/Animation - 1711633209042.json";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Page() {
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.divider}>
-          <View style={styles.left}>
-            <View style={styles.degree}>
-              <Text style={styles.title}>34</Text>
-              <Text style={styles.degreesym}>o</Text>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.divider}>
+            <View style={styles.left}>
+              <View style={styles.degree}>
+                <Text style={styles.title}>34</Text>
+                <Text style={styles.degreesym}>o</Text>
+              </View>
+              <Text style={styles.location}>Kundrathur</Text>
+              <Text style={styles.feels}>35/25 Feellike 39</Text>
             </View>
-            <Text style={styles.subtitle}>Sunny</Text>
+            <LottieView
+              source={require("../assets/Animation - 1711633209042.json")}
+              style={{
+                width: "200%",
+                height: "200%",
+                position: "absolute",
+                left: -50,
+                top: -30,
+              }}
+              autoPlay
+            />
+          </View>
+          <View style={styles.cont}>
+            <View style={styles.div}>
+              <Text style={styles.gray}>Yesterday</Text>
+              <Text style={styles.gray}>35 25</Text>
+            </View>
+            <View style={styles.div}>
+              <View>
+                <Text style={styles.white}>Today</Text>
+                <Text style={styles.white}>Thursday</Text>
+                <Text style={styles.white}>Friday</Text>
+                <Text style={styles.white}>Saturday</Text>
+                <Text style={styles.white}>Sunday</Text>
+              </View>
+              <View>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.spc}>1%</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.spc}>1%</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.spc}>1%</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.spc}>1%</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                  <Text style={styles.spc}>1%</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                  <Text style={styles.spc}>35</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={{ marginTop: 10, marginLeft: 10 }}>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.card2}>Tomorrow</Text>
+              <Text style={styles.card2}>Tomorrow</Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.card2}>Tomorrow</Text>
+              <Text style={styles.card2}>Tomorrow</Text>
+            </View>
+          </View>
+          <View style={styles.card3}>
+            
 
-            <Text style={styles.location}>Kundrathur</Text>
-            <Text style={styles.feels}>35/25 Feels like 39</Text>
-          </View>
-          <View style={styles.right}>
-            <Image source={sunicon} style={styles.icon} />
-          </View>
-        </View>
-        <View style={styles.cont}>
-          <View style={styles.div}>
-            <Text style={styles.gray}>Yesterday</Text>
-            <Text style={styles.gray}>35 25</Text>
-          </View>
-          <View style={styles.div}>
+         
             <View>
-              <Text style={styles.white}>Today</Text>
-              <Text style={styles.white}>Thursday</Text>
-              <Text style={styles.white}>Friday</Text>
-              <Text style={styles.white}>Saturday</Text>
-              <Text style={styles.white}>Sunday</Text>
+              <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
             </View>
-            <View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={styles.spc}>1%</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={styles.spc}>1%</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={styles.spc}>1%</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={styles.spc}>1%</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={styles.spc}>1%</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-                <Text style={styles.spc}>35</Text>
-              </View>
+            <View >
+            <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
+              <Text style={styles.white}>Pollen</Text>
+
             </View>
+          
           </View>
+          <Text style={{color:"gray",textAlign:"center",marginBottom:10}}>Made by Prakash</Text>
         </View>
-        <View style={{marginTop:10,marginLeft:10}}>
-          <View style={{ flexDirection: "row" }}>
-            <Text style={styles.card2}>Tomorrow</Text>
-            <Text style={styles.card2}>Tomorrow</Text>
-          </View>
-          <View style={{flexDirection:"row"}}>
-            <Text style={styles.card2}>Tomorrow</Text>
-            <Text style={styles.card2}>Tomorrow</Text>
-          </View>
-        </View>
-      </View>
+      </ScrollView>
     </>
   );
 }
@@ -92,7 +123,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-
+    overflow: "scroll",
     backgroundColor: "#000000",
   },
   title: {
@@ -113,8 +144,10 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   right: {
-    
-    justifyContent: "start",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    // justifyContent: "start",
   },
   feels: {
     marginTop: 4,
@@ -132,7 +165,6 @@ const styles = StyleSheet.create({
   icon: {
     width: 170,
     height: 170,
-
   },
   cont: {
     marginLeft: 10,
@@ -168,7 +200,6 @@ const styles = StyleSheet.create({
     marginLeft: 16,
   },
   card2: {
-   
     marginRight: 10,
     padding: 20,
     marginTop: 10,
@@ -182,4 +213,17 @@ const styles = StyleSheet.create({
     flex: 1,
     // width:"100%",
   },
+  card3:{
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 20,
+    marginTop: 10,
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+    borderRadius: 10,
+    marginBottom:10
+   
+  }
 });
